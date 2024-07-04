82ebe90116062b26033a9c6d2675b5a66c7cb9b4
sandeepkota312 <sandeepkota312@gmail.com>
2024-07-04 11:05:30 +0530

    lechocolat scraped

--- sites/foreignfortune.py
+++ sites/foreignfortune.py
@@ -4,86 +4,138 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import json
 
+# TODO: to be tested again
 def scrape_product_data(product_url):
-    product_response = requests.get(product_url)
-    product_soup = BeautifulSoup(product_response.text,'html.parser')
-    title = product_soup.find('h1',class_="product-single__title").text.strip()
-    price = product_soup.find('span',id="ProductPrice-product-template").text.strip()
-    description = product_soup.find('div',class_="product-single__description rte").text.strip()
-    product_id = urlparse(product_url).path.split('/')[-1]
     try:
-        main_image = "https:"+product_soup.find('div', class_="product-single__photo js-zoom-enabled product-single__photo--has-thumbnails").get('data-zoom')
-        images = product_soup.find('ul',class_="grid grid--uniform product-single__thumbnails product-single__thumbnails-product-template").find_all('img')
+        product_response = requests.get(product_url)
+        product_response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching product URL: {product_url}, error: {e}")
+        return None
+    
+    product_soup = BeautifulSoup(product_response.text,'html.parser')
 
-        product_images = []
-        for image in images:
-            product_images.append("https:"+image.get('src'))
+    try:
+        title = product_soup.find('h1',class_="product-single__title").text.strip()
+        price = product_soup.find('span',id="ProductPrice-product-template").text.strip()
+        description = product_soup.find('div',class_="product-single__description rte").text.strip()
+        product_id = urlparse(product_url).path.split('/')[-1]
+
+        main_image = "https:" + product_soup.find('div', class_="product-single__photo js-zoom-enabled product-single__photo--has-thumbnails").get('data-zoom')
+        images = product_soup.find('ul', class_="grid grid--uniform product-single__thumbnails product-single__thumbnails-product-template").find_all('img')
+
+        product_images = ["https:" + image.get('src') for image in images]
     except:
-        main_image = "https:"+product_soup.find('div', class_="product-single__photo js-zoom-enabled").get('data-zoom')
-        product_images=[]
+        try:
+            main_image = "https:"+product_soup.find('div', class_="product-single__photo js-zoom-enabled").get('data-zoom')
+            product_images=[]
+        except AttributeError as e:
+            print(f"Error parsing product details for URL: {product_url}, error: {e}")
+            return None
 
+    try:
+        variants = product_soup.find('select', id="ProductSelect-product-template").find_all('option')
+        product_variants = []
+        for variant in variants:
+            data = {"id": variant.get('value')}
+            components_length = len(product_soup.find_all('div', class_="selector-wrapper js product-form__item"))
+            if components_length:
+                components = []
+                for index in range(components_length):
+                    components.append(product_soup.find('label', {'for': f'SingleOptionSelector-{index}'}).text.strip())
 
-    variants= product_soup.find('select',id="ProductSelect-product-template").find_all('option')
-    product_variants=[]
-    for variant in variants:
-        data ={"id":variant.get('value')}
-        components_length = len(product_soup.find_all('div',class_="selector-wrapper js product-form__item"))
-        if components_length:
-            components=[]
-            for index in range(components_length):
-                components.append(product_soup.find('label',{'for':f'SingleOptionSelector-{index}'}).text.strip())
+                values = variant.text.split(' / ')
+                for index, value in enumerate(values):
+                    data[components[index]] = value.strip()
+            product_variants.append(data)
+    except AttributeError as e:
+        print(f"Error parsing product variants for URL: {product_url}, error: {e}")
+        return None
 
-            values = variant.text.split(' / ')
-            for index,value in enumerate(values):
-                data[components[index]]=value.strip()
-        product_variants.append(data)
-
-    product_data={
-        "title":title,
-        "url":product_url,
-        "price":price,
-        "image":main_image,
-        "description":description,
-        "product_id":product_id,
-        "images":product_images,
-        "models":product_variants
+    product_data = {
+        "title": title,
+        "url": product_url,
+        "price": price,
+        "image": main_image,
+        "description": description,
+        "product_id": product_id,
+        "images": product_images,
+        "models": product_variants
     }
     return product_data
 
-def scrape_sections(baseurl,sections_data):
+def scrape_sections(baseurl, sections_data):
     products_data = []
     for each_section_data in sections_data:
-        main_url = baseurl+each_section_data[-1]
-        section_response = requests.get(main_url)
+        main_url = baseurl + each_section_data[-1]
+        try:
+            section_response = requests.get(main_url)
+            section_response.raise_for_status()
+        except requests.RequestException as e:
+            print(f"Error fetching section URL: {main_url}, error: {e}")
+            continue
+        
         section_soup = BeautifulSoup(section_response.text, 'html.parser')
-        total_products =  int("".join(filter(str.isdigit,section_soup.find('span',class_="filters-toolbar__product-count").text.strip())))
-        products_per_page = len(section_soup.find_all('a',class_="grid-view-item__link grid-view-item__image-container product-card__link"))
-        total_pages = math.ceil(total_products/products_per_page)
-        # print(total_pages)
-        for page in range(1,total_pages+1):
-            page_response = requests.get(f"{main_url}?page={page}")
-            page_soup = BeautifulSoup(page_response.text,'html.parser')
-            products = page_soup.find_all('a',class_="grid-view-item__link grid-view-item__image-container product-card__link")
+        
+        try:
+            total_products = int("".join(filter(str.isdigit, section_soup.find('span', class_="filters-toolbar__product-count").text.strip())))
+            products_per_page = len(section_soup.find_all('a', class_="grid-view-item__link grid-view-item__image-container product-card__link"))
+            total_pages = math.ceil(total_products / products_per_page)
+        except (AttributeError, ValueError) as e:
+            print(f"Error parsing section details for URL: {main_url}, error: {e}")
+            continue
+
+        for page in range(1, total_pages + 1):
+            try:
+                page_response = requests.get(f"{main_url}?page={page}")
+                page_response.raise_for_status()
+            except requests.RequestException as e:
+                print(f"Error fetching page URL: {main_url}?page={page}, error: {e}")
+                continue
+            
+            page_soup = BeautifulSoup(page_response.text, 'html.parser')
+            
+            try:
+                products = page_soup.find_all('a', class_="grid-view-item__link grid-view-item__image-container product-card__link")
+            except AttributeError as e:
+                print(f"Error parsing products for URL: {main_url}?page={page}, error: {e}")
+                continue
+            
             for product in products:
                 product_href = product.get('href')
-                print(baseurl+product_href)
-                product_data = scrape_product_data(baseurl+product_href)
-                product_data['category']=each_section_data[0]
-                products_data.append(product_data)
+                product_url = baseurl + product_href
+                print(product_url)
+                product_data = scrape_product_data(product_url)
+                if product_data:
+                    product_data['category'] = each_section_data[0]
+                    products_data.append(product_data)
     return products_data
 
 # Function to scrape Foreign Fortune
 def scrape_foreignfortune(baseurl):
-    response = requests.get(baseurl)
-    if response.status_code != 200:
+    try:
+        response = requests.get(baseurl)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching base URL: {baseurl}, error: {e}")
         return []
+    
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    sections = soup.find('ul',class_='site-nav list--inline site-nav--centered').find_all('a',class_="site-nav__link site-nav__link--main")
-    # print(sections)
+    try:
+        sections = soup.find('ul', class_='site-nav list--inline site-nav--centered').find_all('a', class_="site-nav__link site-nav__link--main")
+    except AttributeError as e:
+        print(f"Error parsing sections for base URL: {baseurl}, error: {e}")
+        return []
+    
     sections_data = []
     for section in sections:
         href = section.get('href')
         text = section.text.strip()
-        sections_data.append([text,href])
-    return scrape_sections(baseurl=baseurl,sections_data=sections_data)
+        sections_data.append([text, href])
+    
+    # HACK: adding shoes section manually as it's not found in the main page
+    sections_data.append(["shoes","/collections/shoes"])
+    sections_data.append(["Foreign kids","/collections/foreign-kids"]) 
+    
+    return scrape_sections(baseurl=baseurl, sections_data=sections_data)
--- sites/lechocolat_alainducasse.py
+++ sites/lechocolat_alainducasse.py
@@ -1,31 +1,92 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
+def scrape_product_data(product_url):
+    try:
+        product_response = requests.get(product_url)
+        product_response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching product URL: {product_url}, error: {e}")
+        return None
+    product_soup = BeautifulSoup(product_response.text,'html.parser')
+
+    try:
+        title = product_soup.find('h1',class_="productCard__title").text.strip()
+        match_price = re.search(r'£\d+\.\d{2}',product_soup.find('button',class_="productActions__addToCart button add-to-cart add").text.strip())
+        price = match_price.group(0) if match_price else None
+        weight = product_soup.find('p',class_="productCard__weight").text.strip()
+        description = product_soup.find('div',class_="productDescription__text wysiwyg-content product-description").text.strip()
+        images = product_soup.find('ul',class_="productImages__list js-product-images-carousel keen-slider js-product-images-list").find_all('li',class_="productImages__item keen-slider__slide")
+        product_images = [image.find('a').get('href') for image in images]
+        try:
+            variant = product_soup.find('p',class_="linkedProducts__title").text.strip()
+        except Exception as e:
+            print(f"varaint doesn't exist, error: {e}")
+            variant=None
+    except Exception as e:
+        print(f"Error parsing product details for URL: {product_url}, error: {e}")
+        return None
+    
+    product_data={
+        "title":title,
+        "url":product_url,
+        "price":price,
+        "images":product_images,
+        "description":description,
+        "weight":weight,
+        "variant":variant
+    }
+    # print(product_data)
+    return product_data
+
+def scrape_types(baseurl,types_data):
+    products_data = []
+    for each_type_data in types_data:
+        main_url = baseurl + each_type_data[-1]
+        type_response = requests.get(main_url)
+        type_soup = BeautifulSoup(type_response.text,'html.parser')
+        products =type_soup.find_all('div',class_='productMiniature js-product-miniature') + type_soup.find_all('div',class_='productMiniature js-product-miniature --oos')
+        # print(len(products))
+        for product in products:
+            product_href = product.find('a').get('href')
+            # print(baseurl,product_href)
+            products_url = [product_href]
+            try:
+                product_response = requests.get(products_url[0])
+                product_response.raise_for_status()
+            except requests.RequestException as e:
+                print(f"Error fetching product URL: {products_url[0]}, error: {e}")
+                return 
+            product_soup = BeautifulSoup(product_response.text,'html.parser')
+            # try:
+            #     sub_products = product_soup.find('ul',class_='linkedProducts__list').find_all('a')
+            #     sub_product_urls=[sub_product.get('href') for sub_product in sub_products]
+            # except Exception as e:
+            #     print(f"Error fetching sub urls, error: {e}")
+            #     sub_product_urls=[]
+            sub_product_urls=[]
+            print("\n")
+            print(products_url+sub_product_urls)
+            print("\n")
+            for product_url in products_url + sub_product_urls:
+                # print(product_url)
+                product_data = scrape_product_data(product_url)
+                if product_data:
+                    product_data['category'] = each_type_data[0]
+                    products_data.append(product_data)
+    return products_data
 
 # Function to scrape Le Chocolat Alain Ducasse
 def scrape_lechocolat_alainducasse(baseurl):
-    response = requests.get(baseurl)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    products = []
-    for product in soup.find_all('li', class_='product-item'):
-        product_id = product.get('data-id', '')
-        name = product.find('a', class_='product-item-link').text.strip()
-        description = product.find('div', class_='description').text.strip()
-        images = [img['data-src'] for img in product.find_all('img')]
-        price = product.find('span', class_='price').text.strip()
-        sale_price = product.find('span', class_='sale-price').text.strip() if product.find('span', class_='sale-price') else ''
-        sku = product.find('span', class_='sku').text.strip() if product.find('span', class_='sku') else ''
-        product_url = product.find('a')['href']
-
-        products.append({
-            "id": product_id,
-            "name": name,
-            "description": description,
-            "images": images,
-            "price": price,
-            "sale_price": sale_price,
-            "sku": sku,
-            "url": product_url
-        })
-    
-    return products
+    response = requests.get(baseurl+"/uk/")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        types = soup.find('ul',class_="homeCategoryPads__list").find_all('a',class_="homeCategoryPads__itemLink")
+        types_data = []
+        for each_type in types:
+            href = each_type['href']
+            text = each_type.find('p',class_="homeCategoryPads__line --top").text.strip() + each_type.find('p',class_="homeCategoryPads__line --bottom").text.strip()
+            types_data.append([text,href])
+        print(types_data)
+    return scrape_types(baseurl=baseurl,types_data=types_data)
